3a4f81044e4e702d1982838f29baebe171143371
nellizi <masterliz9095@gmail.com>
2023-07-13 16:12:35 +0900

    Refactor : 파일 리스트 테이블로 받아오기

--- src/everythingwithswing/AppService.java
+++ src/everythingwithswing/AppService.java
@@ -5,262 +5,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AppService {
-	
-	
 
 	public String[][] getList() {
 		// TODO Auto-generated method stub
+
+//		String c_dir = "C:/";
+//		File mainDir = new File(c_dir); 			// C 하위에 있는 모든 것 찾기-> 내 pc에 있는 모든 파일 찾기
+//		String[] entireFileList = mainDir.list();   // .list() : 파일/디렉토리 명
+//		String[] filenames = mainDir.list();
+//		String filename = mainDir.getName();
+//		String[] number = { "1", "2", "3", "4" };
+//		String[] name = { "A", "B", "C", "D" };
+//		String[] char_ = { "가", "나", "다", "라" };
+//		String[] arr_ = { "a", "b", "c", "d" };
+
 		
-		 String c_dir = "C:/";
-		 File mainDir = new File(c_dir);     //C 하위에 있는 모든 것 찾기-> 내 pc에 있는 모든 파일 찾기 
-		 String[] entireFileList = mainDir.list(); //.list() : 파일/디렉토리 명 
-		 
-		 String[] filenames = mainDir.list();		
-		
-		 String filename = mainDir.getName();
-		 
-		 String[] number = {"1","2","3","4"};
-		 String[] name = {"A","B","C","D"};
-		 String[] char_ = {"가","나","다","라"};
-		 String[] arr_ = {"a","b","c","d"};
-		 
-		 
-		 String[][] data = {{"1","A","가","a"},{"2","B","나","b"},{"3","C","다","c"}};
-		
-		 return data;
-//		 String[][] data =new String[arr_.length][4];
-//		 for(String arr : arr_) {
-//			 data
-//		 }
-//		 
-		 
-		
+		/* 재귀호출로 하위 디렉토리에 있는 파일까지 다 구하기 */
+
+		List<String> fileList = new ArrayList<String>();
+
+		scanDir("C:\\Users\\이지현", fileList);
+
+		String[][] data = new String[fileList.size()][4];
+
+		for (int i = 0; i < fileList.size(); i++) {
+			for (int j = 0; j < 4; j++) {
+				data[i][j] = fileList.get(i);
+			}
+		}
+
+		return data;
+
 	}
-	
-	
-	
-	public void searchFile() {
-		 List<String> fileLst = new ArrayList<String> ();
-			
-			scanDir("C:\\Users\\이지현\\Desktop\\everything", fileLst);
-			
-			for(String fullPath : fileLst) {
-				System.out.println(fullPath);
-			}		
-		}	
-		
-		
-		/**
-		 * 재귀 호출을 이용하여 하위 폴더를 탐색한다 
-		 * @param folderPath
-		 */
-		public static void scanDir(String folderPath, List<String> fileLst) {
-			File[] files = new File(folderPath).listFiles();
-			
-			for(File f : files) {
-				if(f.isDirectory()) {
+
+	/**
+	 * 재귀 호출을 이용하여 하위 폴더를 탐색한다
+	 * 
+	 * @param folderPath
+	 */
+	public static void scanDir(String folderPath, List<String> fileLst) {
+		File[] files = new File(folderPath).listFiles();
+
+		if (files == null) {
+			// Null~Exception발생 -> 파일이 너무 많아서? 권한? 
+		} else {
+
+			for (File f : files) {
+				if (f.isDirectory()) {
 					scanDir(f.getAbsolutePath(), fileLst);
 				} else {
 					fileLst.add(f.getAbsolutePath());
 				}
 			}
-		 
-		 
+		}
+
 	}
-	
-	
-	
-	
-//	
-//	private PreparedStatement psmt;
-//	private Connection con;
-//	private ResultSet rs;
-
-
-
-	// update 
-//	public void updateCustomer(String id, String name, String tel, String address, String grade) {
-//		try {
-//			con = getConnection();
-//			psmt = con.prepareStatement("UPDATE customertb SET name ='" + name + 
-//					"', tel = '" + tel + 
-//					"', address = '" + address + 
-//					"', grade ='" + grade + 
-//					"' WHERE id ='" + id +"' ;");
-//			psmt.executeUpdate();
-//			System.out.println("고객 정보가 변경되었습니다");
-//		} catch(Exception e) {
-//			System.out.println(e.getMessage());
-//		} finally {
-//			close(psmt, con);
-//		}
-//	}
-	
-	// get last ID
-//	public int getLastId() {
-//		try {
-//			con = getConnection();
-//			psmt = con.prepareStatement("SELECT id FROM customertb ORDER BY id DESC LIMIT 1;");
-//			ResultSet rs = psmt.executeQuery();
-//			rs.next();        //ResultSet에서 데이터를 읽을 때 cursor point를 첫 번째 로우에 맞추어야 한다.
-//			return rs.getInt(1);
-//		} catch(Exception e) {
-//			System.out.println(e.getMessage());
-//			return -1; 
-//		} finally {
-//			close(rs, psmt, con);
-//		}
-//	}
-	
-	// delete 
-//	public void deleteCustomer(String id) {
-//		try {
-//			con = getConnection();
-//			psmt = con.prepareStatement("DELETE FROM customertb WHERE id = '" + id + "'");
-//			psmt.executeUpdate();
-//			System.out.println("고객 정보가 삭제되었습니다");
-//		} catch(Exception e) {
-//			System.out.println(e.getMessage());
-//		} finally {
-//			close(psmt, con);
-//		}
-//	}
-
-	// table 데이터 가져오기. JTable이 문자열 2차원 배열을 입력값으로 받기 때문에  
-//	public String[][] getCustomers(){ 
-//		try {
-//			con = getConnection();
-//			psmt = con.prepareStatement("SELECT id, name, tel, address, grade FROM customertb");
-//			ResultSet rs = psmt.executeQuery();
-//			ArrayList<String[]> list = new ArrayList<String[]>();   // 여기부터 잘 이해가 안 된다.
-//			while(rs.next()) {
-//				list.add(new String[] {
-//						rs.getString("id"),
-//						rs.getString("name"),
-//						rs.getString("tel"),
-//						rs.getString("address"),
-//						rs.getString("grade"),
-//				});
-//			}
-//			String[][] arr = new String[list.size()][5];
-//			System.out.println("데이터 불러오기 완료");
-//			return list.toArray(arr);
-//
-//		} catch(Exception e) {
-//			System.out.println(e.getMessage());
-//			return null;
-//		} finally {
-//			close(psmt, con);
-//		}
-//	}
-
-	// table에 데이터 추가
-//	public int createCustomer(String name, String tel, String address, String grade) {
-//		try {
-//			con = getConnection();
-//			psmt = con.prepareStatement(
-//					"INSERT INTO customertb "
-//							+ "(name, tel, address, grade) "
-//							+ "VALUES "
-//							+ "('" + name + "', '" + tel + "', '" + address + "', '" + grade + "')"  
-//					); 
-//			psmt.executeUpdate();
-//			psmt = con.prepareStatement("SELECT LAST_INSERT_ID();");
-//			ResultSet rs = psmt.executeQuery();
-//			rs.next();
-//			System.out.println("고객정보가 추가되었습니다.");
-//			return rs.getInt(1);
-//		} catch(Exception e) {
-//			System.out.println(e.getMessage());
-//			return -1;
-//		} finally {
-//			close(psmt, con);
-//		}
-//	}
-
-	// database table 생성
-//	public void createTable() {
-//		try {
-//			con = getConnection();
-//			psmt = con.prepareStatement(
-//					"CREATE TABLE IF NOT EXISTS "
-//							+ "customertb( id INT NOT NULL AUTO_INCREMENT,"
-//							+ "name VARCHAR(255),"
-//							+ "tel VARCHAR(255),"
-//							+ "address VARCHAR(255),"
-//							+ "grade VARCHAR(255),"
-//							+ "PRIMARY KEY(id) )"
-//					);
-//			psmt.execute();
-//		} catch(Exception e) {
-//			System.out.println(e.getMessage());
-//		} finally {
-//			close(psmt, con);
-//		}
-//	}
-
-	// database 연결
-//	public Connection getConnection() {
-//
-//		try {
-//			Class.forName("com.mysql.cj.jdbc.Driver");
-//			String url = "jdbc:mysql://localhost:3306/sampleDB";
-//			String user = "root";
-//			String pwd = "1234567890";
-//
-//			con = DriverManager.getConnection(url, user, pwd);
-//			System.out.println("DB 연결 완료");
-//			return con;
-//
-//		} catch (ClassNotFoundException e) {
-//			System.out.println("JDBC 드라이버 로드 에러");
-//			return null;
-//		} catch (SQLException e) {
-//			System.out.println("DB 연결 오류");
-//			return null;
-//		}
-//	}
-	
-	
-	// db 연결 종료 - 종료하지 않으면 제어권이 돌아가지 않아 앱이 먹통이 될 수 있다.
-//	public static void close(ResultSet rs, PreparedStatement psmt, Connection conn) {
-//		
-//		try {
-//			if(psmt != null )
-//				psmt.close();
-//			if(conn != null)
-//				conn.close();
-//			if(rs != null)
-//				rs.close();
-//			
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
-	
-//	public static void close(ResultSet rs, PreparedStatement psmt) {
-//		
-//		try {
-//			if(rs != null)
-//				rs.close();
-//			if(psmt != null )
-//				psmt.close();
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
-	
-//	public static void close(PreparedStatement psmt, Connection con) {
-//		
-//		try {
-//			if(psmt != null )
-//				psmt.close();
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
 
 }
--- src/everythingwithswing/Frame.java
+++ src/everythingwithswing/Frame.java
@@ -33,7 +33,7 @@ public class Frame {
 
 	private JFrame frame;
 	private JTextField searchTextField;
-	private DefaultTableModel tableModel;  // 테이블 데이터 편집하기 위해 필요
+	private DefaultTableModel tableModel; // 테이블 데이터 편집하기 위해 필요
 	private JTable table;
 	private JTextField idLabel;
 	private JTextField telLabel;
@@ -56,16 +56,14 @@ public class Frame {
 
 	private JButton btnInsert;
 	private JButton btnDelete;
-	private JButton btnUpdate; 
-	
+	private JButton btnUpdate;
+
 	private int selectedIdx;
 
-	/**
-	 * Launch the application.
-	 */
+	// Launch the application.
 	public static void main(String[] args) {
 
-		EventQueue.invokeLater(new Runnable() {    // ?? invokeLater 찾기
+		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Frame window = new Frame();
@@ -77,88 +75,72 @@ public class Frame {
 		});
 	}
 
-
-
-	/**
-	 * Create the application.
-	 */
+	// Create the application.
 	public Frame() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
+	// Initialize the contents of the frame.
 	private void initialize() {
 
 		appService = new AppService();
 
 		// 패널 배치
-		frame = new JFrame("EveryThing_By_Liz");    						// 메인 프레임
-		frame.setSize(1024, 768);								// 프레임 크기
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// 종료 버튼 누르면 프로그램 종료
-		frame.setLocationRelativeTo(null);						// 프레임을 화면 중앙에 배치
-		frame.setResizable(false);								// 프레임 크기를 변경하지 못하도록 설정
-		frame.getContentPane().setLayout(null);					// 프레임에 추가되는 컴포넌트 레이아웃 -> Absolute
+		frame = new JFrame("EveryThing_By_Liz"); // 메인 프레임
+		frame.setSize(1024, 768); // 프레임 크기
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 종료 버튼 누르면 프로그램 종료
+		frame.setLocationRelativeTo(null); // 프레임을 화면 중앙에 배치
+		frame.setResizable(false); // 프레임 크기를 변경하지 못하도록 설정
+		frame.getContentPane().setLayout(null); // 프레임에 추가되는 컴포넌트 레이아웃 -> Absolute
 
 		panelTop = new JPanel();
-		// 상단 패널
-		panelTop.setBounds(6, 6, 1012, 713);					// 패널 위치와 크기  -> (x좌표, y좌표, 넓이, 높이)
-		panelTop.setLayout(null);								// 상단 패널에 추가되는 컴포넌트 레이아웃 -> Absolute
-		frame.getContentPane().add(panelTop);					// 프레임에 추가하기
-		panelTop.setVisible(true);								// 패널 보이기
 
-		tablePanel = new JPanel();								// 테이블 패널 생성
-		tablePanel.setBounds(20, 50, 972, 650);					// 테이블 패널 위치와 크기
-		frame.getContentPane().add(tablePanel);					// 테이블 패널 추가
+		// 상단 패널
+		panelTop.setBounds(6, 6, 1012, 713); // 패널 위치와 크기 -> (x, y, w, h)
+		panelTop.setLayout(null); // 컴포넌트 레이아웃 -> Absolute
+		frame.getContentPane().add(panelTop);
+		panelTop.setVisible(true); // 패널 보이기
+
+		tablePanel = new JPanel(); // 테이블 패널 생성
+		tablePanel.setBounds(20, 50, 972, 650); // 테이블 패널 위치와 크기
+		frame.getContentPane().add(tablePanel); // 테이블 패널 추가
 
 		// 검색 필드
-		JButton btnSearch = new JButton("검색"); // 검색어 입력 레이블을 버튼으로 구현(버튼 모양이 레이블보다 예뻐서)
-		btnSearch.setBounds(881, 9, 100, 29);         // 검색어 입력 레이블 위치와 크기 -> (x좌표, y좌표, 넓이, 높이)
-		panelTop.add(btnSearch);					 // 상단 패널에 붙이기
-		
-		/////////////////검색
+		JButton btnSearch = new JButton("검색");
+		btnSearch.setBounds(881, 9, 100, 29);
+		panelTop.add(btnSearch); // 상단 패널에 붙이기
 
-		searchTextField = new JTextField();   		 // 검색어 입력 텍스트필드 생성
-		searchTextField.setBounds(12, 9, 857, 30);	 // 검색어 입력 텍스트필드 위치와 크기
-		searchTextField.setColumns(10);				 // 검색어 길이 설정 
-		panelTop.add(searchTextField);				 // 상단 패널에 붙이기
-		
-		searchTextField.addKeyListener(new KeyAdapter() {    // 검색어 입력 텍스트필드 이벤트
+		///////////////// 검색
+		searchTextField = new JTextField(); // 검색어 입력 텍스트필드 생성
+		searchTextField.setBounds(12, 9, 857, 30);
+		searchTextField.setColumns(10); // 검색어 길이 설정
+		panelTop.add(searchTextField); // 상단 패널에 붙이기
+
+		searchTextField.addKeyListener(new KeyAdapter() { // 검색어 입력 텍스트필드 이벤트
 			public void keyReleased(KeyEvent e) {
-				String val = searchTextField.getText();		 
+				String val = searchTextField.getText();
 				TableRowSorter<TableModel> trs = new TableRowSorter<>(table.getModel());
 				table.setRowSorter(trs);
 				trs.setRowFilter(RowFilter.regexFilter(val));
 			}
 		});
 
-		// 이벤트 
+		// 이벤트
 		showTable();
-//		addTableRow();
-//		deleteTableRow();
-//		updateTableRow();
-//		saveData();
-//		cancelDate();
 
 	}
 
-	/** UI테이블 설정
-	 * 
-	 */
+	/** UI테이블 설정 */
 
 	public void showTable() {
 		data = appService.getList();
-//				for(String[] item:data)
-//					for(String ii: item)
-//						System.out.println(ii)
-	
-		String[][] row =new String[][]{{"394116","이지현","123","123"}};
-			
-		String[] header = new String[]{"이름", "경로", "크기", "수정한 날짜"};
 
-		tableModel = new DefaultTableModel(data, header);  
-		table = new JTable(tableModel) {                    // 셀에서 편집할 수 없게 함.
+		String[][] row = new String[][] { { "394116", "이지현", "123", "123" } };
+
+		String[] header = new String[] { "이름", "경로", "크기", "수정한 날짜" };
+
+		tableModel = new DefaultTableModel(data, header);
+		table = new JTable(tableModel) { // 셀에서 편집할 수 없게
 			@Override
 			public boolean isCellEditable(int row, int column) {
 				return false;
@@ -168,16 +150,14 @@ public class Frame {
 
 		// 셀 값 가운데 정렬
 		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-		centerRenderer.setHorizontalAlignment( SwingConstants.CENTER );
-		table.getColumnModel().getColumn(0).setCellRenderer( centerRenderer );
-		table.getColumnModel().getColumn(1).setCellRenderer( centerRenderer );
-		table.getColumnModel().getColumn(2).setCellRenderer( centerRenderer );
-		table.getColumnModel().getColumn(3).setCellRenderer( centerRenderer );
+		centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+		table.getColumnModel().getColumn(0).setCellRenderer(centerRenderer);
+		table.getColumnModel().getColumn(1).setCellRenderer(centerRenderer);
+		table.getColumnModel().getColumn(2).setCellRenderer(centerRenderer);
+		table.getColumnModel().getColumn(3).setCellRenderer(centerRenderer);
 		TableCellRenderer rendererFromHeader = table.getTableHeader().getDefaultRenderer();
 		JLabel headerLabel = (JLabel) rendererFromHeader;
 		headerLabel.setHorizontalAlignment(JLabel.CENTER);
-		
-		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);  //행을 하나만 선택할 수 있도록
 
 		// 컬럼 크기
 		TableColumnModel colModel = table.getColumnModel();
@@ -186,22 +166,18 @@ public class Frame {
 		colModel.getColumn(2).setPreferredWidth(15);
 		colModel.getColumn(3).setPreferredWidth(100);
 
-		table.getTableHeader().setFont(new Font("NanumGothic", Font.BOLD, 13));  // header 폰트 설정
-		table.getTableHeader().setPreferredSize(new Dimension(100, 30));		// header 넓이, 높이 
-		table.setFont(new Font("NanumGothic", Font.PLAIN, 13));                 // 셀 폰트, 크기
+		table.getTableHeader().setFont(new Font("NanumGothic", Font.BOLD, 13)); // header 폰트 설정
+		table.getTableHeader().setPreferredSize(new Dimension(100, 30)); // header 넓이, 높이
+		table.setFont(new Font("NanumGothic", Font.PLAIN, 13)); // 셀 폰트, 크기
 		table.setRowHeight(30);
-//		table.setAlignmentX(0);
-		
+
 		// 테이블에 스크롤바 추가
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setPreferredSize(new Dimension(975, 645));
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 0));	// padding(상, 좌, 하, 우)
+		scrollPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 0)); // padding(상, 좌, 하, 우)
 		tablePanel.add(scrollPane); // JScrollPane을 panelTop에 바로 올리면 안 보임. 전용 tablePanel에 올려야 보임
 
 	}
-
-	
-
 
 }
